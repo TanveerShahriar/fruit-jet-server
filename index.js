772418cb5fd98c534ecae887bfed6f04fcfef9dd
TanveerShahriar <tanveershahriara@gmail.com>
2022-05-01 02:29:59 +0600

    delete added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -8,7 +8,7 @@ const app = express();
 
 // middleware
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.31xws.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -23,12 +23,20 @@ async function run() {
     await client.connect();
     const fruitsCollection = client.db("fruitJet").collection("fruits");
 
-    // Fruits
+    // Get fruits
     app.get("/inventory", async (req, res) => {
       const query = {};
       const cursor = fruitsCollection.find(query);
       const fruits = await cursor.toArray();
       res.send(fruits);
+    });
+
+    // Delete fruits
+    app.delete("/inventory/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await fruitsCollection.deleteOne(query);
+      res.send(result);
     });
   } finally {
   }
