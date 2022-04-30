97ba4cb7a60d2cb299875c4b40db44ada21006a9
TanveerShahriar <tanveershahriara@gmail.com>
2022-05-01 00:16:09 +0600

    get inventory added

--- index.js
+++ index.js
@@ -4,37 +4,42 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
-const app = express()
+const app = express();
 
-// middleware 
+// middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); 
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.31xws.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverApi: ServerApiVersion.v1,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
 async function run() {
-    try {
-        // Connect the client to the server
-        await client.connect();
-        // Establish and verify connection
-        await client.db("fruitJet").command({ ping: 1 });
-        console.log("Connected successfully to server");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
-    }
+  try {
+    await client.connect();
+    const fruitsCollection = client.db("fruitJet").collection("fruits");
+
+    // Fruits
+    app.get("/inventory", async (req, res) => {
+      const query = {};
+      const cursor = fruitsCollection.find(query);
+      const fruits = await cursor.toArray();
+      res.send(fruits);
+    });
+  } finally {
+  }
 }
+
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-    res.send("Running Server");
+  res.send("Running Server");
 });
 
 app.listen(port, () => {
-    console.log("Listening to port", port);
+  console.log("Listening to port", port);
 });
